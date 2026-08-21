013ef4a0db12b3032a07a932af389c540371a9f8
Davi <davids4@al.insper.edu.br>
2026-08-21 09:20:59 -0300

    edit de notes.

--- servidor.py
+++ servidor.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template_string, request, redirect
 import views
-import sqlite3
 
 
 app = Flask(__name__)
@@ -23,16 +22,25 @@ def submit_form():
 
 @app.route('/delete/<int:id>', methods=['GET'])
 def delete(id):
-
-    conexao = sqlite3.connect("banco.db")
-    cursor = conexao.cursor()
-
-    cursor.execute(f"DELETE FROM note WHERE id={id}")
-
-    conexao.commit()
-    cursor.close()
+    views.delete(id)
     return redirect('/')
 
+@app.route('/update/<int:id>', methods=['GET'])
+def edit(id):
+    page = views.edit(id)
+    if page is None:
+        return redirect('/')
+
+    return render_template_string(page)
+
+@app.route('/update', methods=['POST'])
+def update():
+    id = request.form.get('id', type=int)
+    titulo = request.form.get('titulo')
+    detalhes = request.form.get('detalhes')
+
+    views.update(id, titulo, detalhes)
+    return redirect('/')
 
     
 
--- views.py
+++ views.py
@@ -1,4 +1,11 @@
-from utils import load_data, load_template, adicionar_anotacao
+from utils import (
+    adicionar_anotacao,
+    apagar_anotacao,
+    atualizar_anotacao,
+    buscar_anotacao,
+    load_data,
+    load_template,
+)
 
 def index():
     note_template = load_template('components/note.html')
@@ -18,3 +25,20 @@ def submit(titulo, detalhes):
     }
 
     adicionar_anotacao(params)
+
+def delete(id):
+    apagar_anotacao(id)
+
+def edit(id):
+    note = buscar_anotacao(id)
+    if note is None:
+        return None
+
+    return load_template('edit.html').format(
+        id=note['id'],
+        title=note['titulo'],
+        details=note['detalhes'],
+    )
+
+def update(id, titulo, detalhes):
+    atualizar_anotacao(id, titulo, detalhes)
